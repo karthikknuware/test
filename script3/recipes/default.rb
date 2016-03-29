bc022257c391f81e7b7b39c324d2285239939e2f
karthikknuware <root@Nusys-286.nuware.com>
2016-03-29 10:13:08 +0530

    done

--- script3/recipes/default.rb
+++ script3/recipes/default.rb
@@ -3,6 +3,6 @@ bash "install_something" do
   cwd "/tmp"
   code <<-EOH
     curl -O https://www.loggly.com/install/configure-linux.sh
-    sudo bash configure-linux.sh -a USERNAME -r
+    sudo bash configure-linux.sh -a karthik -r
   EOH
   end
